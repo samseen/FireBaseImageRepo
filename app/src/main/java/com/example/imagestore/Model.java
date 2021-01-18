f3b509b77bce4c77556fe344648dab119ab69280
Samson Akanbi <samson.akanbi@outlook.com>
2021-01-18 22:31:46 +0100

    Add place address to the model.

--- app/src/main/java/com/example/imagestore/Model.java
+++ app/src/main/java/com/example/imagestore/Model.java
@@ -3,19 +3,25 @@ package com.example.imagestore;
 public class Model {
 
     private String imageUrl;
+    private String placeAddress;
     public Model() {
 
     }
 
-    public Model(String imageUrl) {
+    public Model(String imageUrl, String placeAddress) {
         this.imageUrl = imageUrl;
+        this.placeAddress = placeAddress;
     }
 
     public String getImageUrl() {
         return imageUrl;
     }
 
+    public String getPlaceAddress() { return placeAddress; }
+
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
     }
+
+    public void setPlaceAddress(String placeAddress) { this.placeAddress = placeAddress; }
 }
